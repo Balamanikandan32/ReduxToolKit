b08d38a758cf44322eb70c21221d8b22322726e9
Balamanikandan32 <bala34510@gmail.com>
2024-07-31 16:16:14 +0530

    Configure store, and uses store fucntions

--- src/Components/ReduxToolKit.js
+++ src/Components/ReduxToolKit.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 const ReduxToolKit = ()=> {
     return(
@@ -23,6 +23,20 @@ const cakeSlice = createSlice({
         }
     }
 })
+
+const store = configureStore({
+    reducer:{
+        cake:cakeSlice.reducer
+    },
+})
+
+
+console.log("initial state",store.getState())
+const unsubscribe = store.subscribe(()=> console.log("updated state",store.getState()))
+
+store.dispatch(cakeSlice.actions.ordered())
+store.dispatch(cakeSlice.actions.restocked(5))
+
 
 export const {ordered, restocked}=cakeSlice.actions
 export default cakeSlice.reducer
