9fe36a0252072c949f7f1e4d044cf4cef2985693
Balamanikandan32 <bala34510@gmail.com>
2024-08-02 11:44:30 +0530

    React- Redux ToolKit provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {ReduxToolKit} from "./Components/ReduxToolKit"
-import CakeView from "./Components/CakeView"
-import IcecreamView from "./Components/IcecreamView"
-import UserView from "./Components/UserView"
+import { ReduxToolKit } from "./Components/ReduxToolKit";
+import CakeView from "./Components/CakeView";
+import IcecreamView from "./Components/IcecreamView";
+import UserView from "./Components/UserView";
+import { Provider } from "react-redux"
+import store from "./App/Store"
 
 function App() {
   return (
-    <div className="App">
-      <CakeView />
-      <IcecreamView />
-      <UserView />
-    </div>
+    <Provider store = {store}>
+      <div className="App">
+        <CakeView />
+        <IcecreamView />
+        <UserView />
+      </div>
+    </Provider>
   );
 }
 
