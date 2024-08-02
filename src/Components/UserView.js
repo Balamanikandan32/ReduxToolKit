9e46bc31f3799d9c7ca87f132d8bed66f4e360f5
Balamanikandan32 <bala34510@gmail.com>
2024-08-02 12:24:04 +0530

    Async in react-REdux Toolkit

--- src/Components/UserView.js
+++ src/Components/UserView.js
@@ -1,11 +1,23 @@
-
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../Features/Users/userSlice";
 
 const UserView = () => {
+  const dispatch = useDispatch();
+  const { loading, users, error } = useSelector((state) => state.users);
+  useEffect(() => async() => dispatch(fetchUsers()), []);
   return (
-    <div>Users</div>
-    
-  )
-}
+    <div>
+      <h2>Users</h2>
+      {loading ? (
+        <>Loading</>
+      ) : users ? (
+        users?.map((item, index) => <li key={index}>{item.name}</li>)
+      ) : (
+        <>{error}</>
+      )}
+    </div>
+  );
+};
 
-export default UserView
+export default UserView;
--- src/Features/Users/userSlice.js
+++ src/Features/Users/userSlice.js
@@ -6,7 +6,7 @@ const initialUserState = {
   error: "",
 };
 
-const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+export const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   return response.json();
 });
