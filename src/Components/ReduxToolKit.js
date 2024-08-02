f2e63c60326c71e3b92e440ba40a5e30b0d2d1ce
Balamanikandan32 <bala34510@gmail.com>
2024-08-02 10:59:52 +0530

    createAsyncThunk

--- src/Components/ReduxToolKit.js
+++ src/Components/ReduxToolKit.js
@@ -1,68 +1,100 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
 
-const ReduxToolKit = ()=> {
-    return(
-        <div>
-            <h1>ReduxToolKit</h1>
-        </div>
-    )
-}
+const ReduxToolKit = () => {
+  return (
+    <div>
+      <h1>ReduxToolKit</h1>
+    </div>
+  );
+};
 const initialCakeState = {
-    numOfCakes : 10,
-}
+  numOfCakes: 10,
+};
 
 const initialIcecreamState = {
-    numOfIcecream : 20,
-}
+  numOfIcecream: 20,
+};
+
+const initialUserState = {
+  loading: true,
+  users: [],
+  error: "",
+};
+
+const fetchUsers = createAsyncThunk("users/fetchUsers", async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  return response.json();
+});
 
 const cakeSlice = createSlice({
-    name: "cake",
-    initialState:initialCakeState,
-    reducers : {
-        ordered: (state,action)=>{
-            state.numOfCakes--
-        },
-        restocked : (state,action)=>{
-            state.numOfCakes+=action.payload
-        }
-    }
-})
+  name: "cake",
+  initialState: initialCakeState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numOfCakes--;
+    },
+    restocked: (state, action) => {
+      state.numOfCakes += action.payload;
+    },
+  },
+});
 
 const IcecreamSlice = createSlice({
-    name: "icecream",
-    initialState:initialIcecreamState,
-    reducers: {
-        ordered:(state,action)=>{
-            state.numOfIcecream--
-        },
-        restocked:(state,action)=>{
-            state.numOfIcecream+= action.payload
-        }
+  name: "icecream",
+  initialState: initialIcecreamState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numOfIcecream--;
     },
-    extraReducers: (builder)=>{
-        builder.addCase(cakeSlice.actions.ordered,(state,action)=>{
-            state.numOfIcecream--
-        })
-    }
-})
+    restocked: (state, action) => {
+      state.numOfIcecream += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase("cake/ordered", (state, action) => {
+      state.numOfIcecream--;
+    });
+  },
+});
+
+const userSlice = createSlice({
+  name: "users",
+  initialState: initialUserState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchUsers.pending, (state) => {state.loading = true})
+      .addCase(fetchUsers.fulfilled, (state, action) => {
+        state.loading = false; state.users = action.payload;
+      })
+      .addCase(fetchUsers.rejected,(state,action)=>{
+        state.loading = false; state.error = action.error.message;
+    })
+  },
+});
 const store = configureStore({
-    reducer:{
-        cake:cakeSlice.reducer,
-        icecream: IcecreamSlice.reducer,
-    },
-})
+  reducer: {
+    cake: cakeSlice.reducer,
+    icecream: IcecreamSlice.reducer,
+    users:userSlice.reducer,
+  },
+});
 
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
 
-console.log("initial state",store.getState())
-const unsubscribe = store.subscribe(()=> console.log("updated state",store.getState()))
+store.dispatch(cakeSlice.actions.ordered());
+store.dispatch(cakeSlice.actions.restocked(5));
+store.dispatch(IcecreamSlice.actions.ordered());
+store.dispatch(IcecreamSlice.actions.restocked(3));
 
-store.dispatch(cakeSlice.actions.ordered())
-store.dispatch(cakeSlice.actions.restocked(5))
-store.dispatch(IcecreamSlice.actions.ordered())
-store.dispatch(IcecreamSlice.actions.restocked(3))
+store.dispatch(fetchUsers())
 
-
-
-export const {ordered, restocked}=cakeSlice.actions
-export default cakeSlice.reducer
-export {ReduxToolKit};
+export const { ordered, restocked } = cakeSlice.actions;
+export default cakeSlice.reducer;
+export { ReduxToolKit };
