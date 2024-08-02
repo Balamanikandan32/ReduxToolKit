2caf7a49115fffdacf6617c78d0f9adec9f2643c
Balamanikandan32 <bala34510@gmail.com>
2024-08-02 12:08:25 +0530

    input box for restock (action payload)

--- src/Components/IcecreamView.js
+++ src/Components/IcecreamView.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ordered, restocked } from '../Features/Icecream/icecreamSlice'
 
 const IcecreamView = () => {
+    const[restockIcecream, setRestockIcecream] = useState(1)
     const numOfIcecream =useSelector(state => state.icecream.numOfIcecream)
     const dispatch = useDispatch()
   return (
     <div>
       <h2>Number of Icecreams - {numOfIcecream}</h2>
       <button onClick={()=> dispatch(ordered())}>Buy Icecream</button>
-      <button onClick={()=> dispatch(restocked(10))}>Restock Icecream</button>
+      <input name = "restockIcecream" value ={restockIcecream} onChange={(e)=> setRestockIcecream(e?.target?.value)} />
+      <button onClick={()=> dispatch(restocked(parseInt(restockIcecream)))}>Restock Icecream</button>
     </div>
   ) 
 }
